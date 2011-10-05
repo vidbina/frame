69d852115de0632b29e2d7c635696840a8e98d84
David Asabina <david.asabina@gmail.com>
2011-10-05 10:04:57 +0200

    getting a few Page things right

--- test/PageTest.php
+++ test/PageTest.php
@@ -1,11 +1,12 @@
 <?php
 require_once("config.php");
-require_once(FRAME_PATH.Page);
+require_once(FRAME_PATH.view.Page);
+require_once(FRAME_PATH.view.templates.HTML5);
 
 class PageTest extends PHPUnit_Framework_TestCase {
 	private $object;
 
-	public function setup(){
+	public function setUp(){
 		$this->object = new frame\Page();
 	}
 
@@ -13,27 +14,35 @@ class PageTest extends PHPUnit_Framework_TestCase {
 
 	// should throw exception when Template is not set
 	/**
-	 * @expectedException PageException
+	 * @expectedException frame\PageException
 	 * @expectedExceptionMessage template not set
 	 */
 	public function testExceptionTemplateNotSet() {
 		$this->object->render();
 	}
 
-	// should throw exception when Content is not set
+	// setting invalid template
 	/**
-	 * @expectedException PageException
-	 * @expectedExceptionMessage no content 
+	 * @expectedException frame\PageException
+	 * @expectedExceptionMessage invalid template
 	 */
-	public function testExceptionNoContent() {
+	public function testInvalidTemplate() {
 		$this->object->setTemplate("x");
-		$this->object->render();
-		// TODO: add code
 	}
 
+	// page rendering with valid template
+	public function testValidTemplate() {
+		$template = new frame\HTML5Template();
+		$this->object->setTemplate($template);
+		$this->assertTrue($this->object->render());
+	}
+	
 	// should have a settable title through getter and setter methods
 	public function testTitleSetterandGetter() {
 		// TODO: add code
+		$string = "flippedyfloppidyfloop";
+		$this->object->setTitle($string);
+		$this->assertEquals($string, $this->object->getTitle());
 	}
 	
 	// should have a settable Template through getter and setter methods
