f0e73aa7911d821855c8c32a4af98ea9adb6b098
David Asabina <david.asabina@gmail.com>
2011-10-05 18:16:45 +0200

    the test branch is broken

--- test/PageTest.php
+++ test/PageTest.php
@@ -1,7 +1,6 @@
 <?php
 require_once("config.php");
 require_once(FRAME_PATH.view.Page);
-require_once(FRAME_PATH.view.templates.HTML5);
 
 class PageTest extends PHPUnit_Framework_TestCase {
 	private $object;
@@ -14,35 +13,27 @@ class PageTest extends PHPUnit_Framework_TestCase {
 
 	// should throw exception when Template is not set
 	/**
-	 * @expectedException frame\PageException
+	 * @expectedException PageException
 	 * @expectedExceptionMessage template not set
 	 */
 	public function testExceptionTemplateNotSet() {
 		$this->object->render();
 	}
 
-	// setting invalid template
+	// should throw exception when Content is not set
 	/**
-	 * @expectedException frame\PageException
-	 * @expectedExceptionMessage invalid template
+	 * @expectedException PageException
+	 * @expectedExceptionMessage no content 
 	 */
-	public function testInvalidTemplate() {
+	public function testExceptionNoContent() {
 		$this->object->setTemplate("x");
+		$this->object->render();
+		// TODO: add code
 	}
 
-	// page rendering with valid template
-	public function testValidTemplate() {
-		$template = new frame\HTML5Template();
-		$this->object->setTemplate($template);
-		$this->assertTrue($this->object->render());
-	}
-	
 	// should have a settable title through getter and setter methods
 	public function testTitleSetterandGetter() {
 		// TODO: add code
-		$string = "flippedyfloppidyfloop";
-		$this->object->setTitle($string);
-		$this->assertEquals($string, $this->object->getTitle());
 	}
 	
 	// should have a settable Template through getter and setter methods
